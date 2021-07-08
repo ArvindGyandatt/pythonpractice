44b1dce3383677390883ad3cce9ddfe3e283041f
ArvindGyandatt <86712019+ArvindGyandatt@users.noreply.github.com>
2021-07-09 01:24:02 +0530

    Create accum.py

---
+++ accum.py
@@ -0,0 +1,15 @@
+s='abcd'
+ans = ''
+i = 0
+while i < len(s):
+    n = 0
+    while n < i+1:
+        if n == 0:
+            ans += s[i].upper()
+        else:
+            ans += s[i].lower()
+        n += 1
+    ans += '-'
+    i += 1
+
+print(ans[:len(ans)-1])
