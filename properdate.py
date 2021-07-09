b49bc4a51152c55351f3ce99a82fdd2b2296ba42
ArvindGyandatt <86712019+ArvindGyandatt@users.noreply.github.com>
2021-07-09 11:14:09 +0530

    Update properdate.py

--- properdate.py
+++ properdate.py
@@ -1,13 +1,36 @@
-days=45
-month=8
-year=2018
-if(days>31):
-    month+=1
-    days=days-31
+ d='49/07/2028'
+ a= d.split("/")
+ days = int(a[0])
+ month = int(a[1])
+ years = int(a[2])
+ 
 
-if(month>12):
-    years+=1
-    month=month-12
+ m31=[1,3,5,7.8,10,12]
+ m30=[4,6,9,11]
 
-date=str(days)+":"+str(month)+":"+str(year)
-print(date)
+ days=int(a[0])
+ month=int(a[1])
+ year=int(a[2])
+
+ if month in m31:
+     if(days>31):
+         month+=1
+         days=days-31
+
+ elif month in m30:
+     if(days>30):
+         month+=1
+         days=days-30
+
+ elif month == 2:
+     if(days>28):
+         month+=1
+         days=days-28
+
+
+ if(month>12):
+     years+=1
+     month=month-12
+
+ date=str(days)+"/"+str(month)+"/"+str(year)
+ print(date)
